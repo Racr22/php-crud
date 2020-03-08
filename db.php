ebf04cf403fc85f3169f056583e95341e587cfe2
Rodrigo Cerezo <cerezorodrigo@gmail.com>
2020-03-08 17:31:34 -0300

    Update db.php

--- db.php
+++ db.php
@@ -6,4 +6,10 @@
         '5e3208fb',
         'heroku_dde248a1a85b114'
     );
+if(!$conexion){
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 ?>
