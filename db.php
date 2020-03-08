27f7cc836e4df3a0473260a92790e16997b80e01
Rodrigo Cerezo <cerezorodrigo@gmail.com>
2020-03-08 16:56:56 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
     $conexion= mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'php_mysql_crud'
+        'us-cdbr-iron-east-04.cleardb.net',
+        'b2ced8a60c1f5b',
+        '5e3208fb',
+        'heroku_dde248a1a85b114'
     );
 ?>
